ae0bfa3b2ea8f21cb6d2901bfedefeee3ce57f4a
Ankit Das <ankitsonu240@gmail.com>
2023-08-17 23:57:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Card from './components/Card'
+import data from './data'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  const cards = data.map(item => {
+    return (
+      <Card
+        key={item.id}
+        item={item}
+      />
+    )
+  })
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
+    </>
+  )
+}
+
+export default App
